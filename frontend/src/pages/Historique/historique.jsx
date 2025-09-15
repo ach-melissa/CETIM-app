f61dba1d10cf0ffc78a30160a446f62980a38178
ach-melissa <infomely05@gmail.com>
2025-09-15 23:08:00 +0200

    new

--- frontend/src/pages/Historique/historique.jsx
+++ frontend/src/pages/Historique/historique.jsx
@@ -5,7 +5,6 @@ import React from 'react';
 export default function Historique() {
   return (
     <div style={{ padding: '20px' }}>
-        <Header />
       <h1>Hello Historique</h1>
     </div>
   );
